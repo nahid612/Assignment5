c292b5fb49719c01a12293dc5292f1e77eef9934
Nahidul Akbar <130125840+nahid612@users.noreply.github.com>
2024-02-19 23:04:49 +0600

    final commit

--- script/append.js
+++ script/append.js
@@ -1,58 +1,3 @@
-// const buttons = document.querySelectorAll('button');
-// // for(let i = 0; i < buttons.length; i++){
-// //     buttons[i].disabled = i < 36;
-// // }
-// let totalResult = 0;
-
-// function calculateTicketFee(event){
-//     const result = event * 550;
-//     // console.log(result);
-
-//     const p = document.createElement('p');
-//     p.textContent = `Result for Button ${event}: ${result}`;
-
-//     document.body.appendChild(p);
-//      totalResult += result;
-
-//      updateTotalResult();
-
-//     //  button color change
-//     // buttons[event].classList.add('button-green')
-
-// }
-
-// function updateTotalResult(){
-//     const totalP = document.getElementById('totalResult');
-//     totalP.textContent = `Total: ${totalResult}`;
-// }
-
-
-
-// ----------------------------
-// const buttons = document.querySelectorAll('button');
-// // for(let i = 0; i < buttons.length; i++){
-//     // buttons[i].disabled = i < 36;
-// // }
-// let totalResult = 0;
-
-// function calculateTicketFee(event){
-//     const result = event * 550;
-//     console.log(result);
-
-//     const p = document.createElement('p');
-//     p.textContent = `Result for Button ${event}: ${result}`;
-
-//     document.body.appendChild(p);
-//      totalResult += result;
-
-//      updateTotalResult();
-
-// }
-
-// function updateTotalResult(){
-//     const totalP = document.getElementById('totalResult');
-//     totalP.textContent = `Total: ${totalResult}`;
-// }
 
 const allBtn = document.getElementsByClassName("allbtn");
 let count = 0;
@@ -65,7 +10,7 @@ for (const btn of allBtn) {
     const sitName = event.target.parentNode.childNodes[3].innerText;
     const sitPrice = event.target.parentNode.childNodes[1].childNodes[3];
     // console.log(event.target.childNodes[0])
-    // console.log()
+    // console.log(event.target.parentNode.childNodes[1].childNodes[3])
 
     const appendOutput = document.getElementById("append-section");
     const li = document.createElement("li");
@@ -82,10 +27,10 @@ for (const btn of allBtn) {
     li.appendChild(p3)
     appendOutput.appendChild(li);
 
-    // const totalCost = document.getElementById('total-result').innerText;
+    const totalCost = document.getElementById('total-result').innerText;
 
-    // const convertedTotalCost = parseInt(totalCost);
-    // document.getElementById('total-result').innerText = convertedTotalCost + parseInt(sitPrice);
+    const convertedTotalCost = parseInt(totalCost);
+    document.getElementById('total-result').innerText = convertedTotalCost + parseInt(sitPrice);
 
     setInnerText("cart-count", count);
   });
